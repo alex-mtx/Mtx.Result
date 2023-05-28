66ff1453f4a5b077e6909f33dba7796d9a7e91cc
alex mtx <alex-mtx@users.noreply.github.comx>
2023-05-29 01:10:27 +0200

    enhancing Cosmos services

---
+++ Mtx.Results/DataResult.cs
@@ -0,0 +1,32 @@
+﻿namespace Mtx.Results;
+
+public record DataResult<T>
+{
+	private readonly Result result;
+
+	public DataResult(int StatusCode, T? Contents = default, string Message = "", Exception? Exception = null)
+	{
+		result = new Result(StatusCode, Message, Exception);
+		this.Contents = Contents;
+	}
+
+
+	public bool HasValue => Contents is not null;
+	public T? Contents { get; }
+
+	public bool IsError => result.IsError;
+	public bool IsSuccess => result.IsSuccess;
+	public bool HasException => result.HasException;
+	public bool IsSuccessAndHasValue => IsSuccess && HasValue;
+
+
+	public static DataResult<T> Ok200(T contents) => new(Status200OK, Contents:contents);
+	public static DataResult<T> NotFound404() => new(Status404NotFound);
+	public static DataResult<T> BadRequest400(string error = "") => new(Status400BadRequest, Message: error);
+	public static DataResult<T> InternalError(string error = "", Exception? exception = default) 
+		=> new(Status500InternalServerError, Message: error, Exception: exception);
+
+	public static implicit operator T?(DataResult<T> result) => result.Contents;
+
+}
+
--- Mtx.Results/Result.cs
+++ Mtx.Results/Result.cs
@@ -7,20 +7,8 @@ public record Result(int StatusCode, string Message = "", Exception? Exception =
 	public bool HasException => Exception is not null;
 	public static Result Ok200() => new(Status200OK);
 	public static Result NoContent204() => new(Status204NoContent);
-	public static Result BadRequest400() => new(Status400BadRequest);
-	public static Result InternalError(string message = "", Exception? exception = default) => new(Status500InternalServerError, message, exception);
+	public static Result BadRequest400(string error = "") => new(Status400BadRequest, Message: error);
+	public static Result NotFound404() => new(Status404NotFound);
+	public static Result InternalError(string error = "", Exception? exception = default) => new(Status500InternalServerError, error, exception);
 	public static implicit operator int(Result result) => result.StatusCode;
 }
-
-public record DataResult<T> : Result where T : class
-{
-	public DataResult(int StatusCode, T? Contents = default, string Message = "", Exception? Exception = null) : base(StatusCode, Message, Exception)
-	{
-		this.Contents = Contents;
-	}
-	public bool HasValue => Contents is not null;
-	public T? Contents { get; }
-	public static implicit operator T?(DataResult<T> result) => result.Contents;
-
-}
-
